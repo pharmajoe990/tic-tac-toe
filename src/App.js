468f682c13e51777aa9a28763def2d61ef5e9c37
Tim Roper <tim.roper@interfisystems.com>
2016-11-28 13:06:03 +1100

    Refactored to use Map for rendering the board

--- src/App.js
+++ src/App.js
@@ -10,30 +10,21 @@ function Square(props) {
   );
 }
 
-class Board extends React.Component {  
+class Board extends React.Component {    
+
   renderSquare(i) {    
     return <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)} />;        
   }
 
   render() {
+    // For each row map to sum of row + column
     return (
       <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {[0, 3, 6].map(row => 
+          <div className="board-row">
+            {[0, 1, 2].map(col => (this.renderSquare(row + col)))}
+          </div>
+        )}
       </div>
     );
   }
